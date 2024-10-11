266539dda6d05c554eef2a802b98866d5d2b62d3
rubaiiyat <abirr1516@gmail.com>
2024-10-11 11:55:51 +0600

    Accounts model update and add with admin

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,15 @@ from .constants import ACCOUNT_TYPE, GENDER_TYPE
 
 class UserBankAccount(models.Model):
     user = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
+    account_no = models.IntegerField(unique=True, null=False, blank=False, default=None)
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE)
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=20, choices=GENDER_TYPE)
     initial_deposit_date = models.DateField(auto_now_add=True)
-    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    def __str__(self) -> str:
+        return f"{self.account_no}"
 
 
 class userAddress(models.Model):
@@ -18,3 +22,6 @@ class userAddress(models.Model):
     city = models.CharField(max_length=50)
     postal_code = models.IntegerField()
     country = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.user.email}"
