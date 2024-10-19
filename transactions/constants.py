643c24477789afefec58dc1774f0856b1460a4f6
rubaiiyat <abirr1516@gmail.com>
2024-10-19 16:24:05 +0600

    Add deposit withdraw load form

---
+++ transactions/constants.py
@@ -0,0 +1,6 @@
+TRANSACTION_TYPE = (
+    (1, "Deposit"),
+    (2, "Withdrawal"),
+    (3, "Load"),
+    (4, "Load Paid"),
+)
---
+++ transactions/forms.py
@@ -0,0 +1,68 @@
+from typing import Any, Mapping
+from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from .models import Transaction
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ("amount", "transaction_type")
+
+    def __init__(self, *args, **kwargs):
+        self.account = kwargs.pop("account")
+        super().__init__(*args, **kwargs)
+        self.fields["transaction_type"].disabled = True
+        self.fields["transaction_type"].widget = forms.HiddenInput()
+
+    def save(self, commit=True):
+        self.instance, account = self.account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save()
+
+
+class DepositForm(TransactionForm):
+    def clean_amount(self):
+        min_deposit_amount = 100
+        amount = self.cleaned_data.get("amount")
+
+        if amount < min_deposit_amount:
+            raise forms.ValidationError(
+                f"You need to deposit at least {min_deposit_amount}"
+            )
+
+        return amount
+
+
+class WithdrawForm(TransactionForm):
+    def clean_amount(self):
+        account = self.account
+        min_withdraw_amount = 500
+        max_withdraw_amount = 2000
+        balance = account.balance
+        amount = self.cleaned_data.get("amount")
+        if amount < min_withdraw_amount:
+            raise forms.ValidationError(
+                f"You can withdraw at least {min_withdraw_amount}"
+            )
+
+        if amount > max_withdraw_amount:
+            raise forms.ValidationError(
+                f"You can withdraw at most {max_withdraw_amount}"
+            )
+
+        if amount > balance:
+            raise forms.ValidationError(
+                f"You have ${balance} in your account"
+                "You Can't withdraw more then your account balance"
+            )
+        return amount
+
+
+class LoadRequestForm(TransactionForm):
+    def clean_amount(self):
+        amount = self.cleaned_data.get("amount")
+
+        return amount
